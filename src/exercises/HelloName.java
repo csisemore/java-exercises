f7b9ab41271f1c628eb5cf18d2068bc4986fe392
csisemore <43246653+csisemore@users.noreply.github.com>
2019-01-07 13:06:05 -0600

    Moving items back to new repository

---
+++ src/exercises/HelloName.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+
+public class HelloName {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String person;
+
+        in = new Scanner(System.in);
+        //do {
+        System.out.print("Enter your name: ");
+            person = in.nextLine();
+        //}
+
+
+        System.out.println("hello " + person);
+
+    }
+}
+
+
